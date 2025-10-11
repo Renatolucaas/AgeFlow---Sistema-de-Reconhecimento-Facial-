d4f383725fc487dee2875495e8da4bb1ce209dcb
Renato Lucas <renatolucas@ucl.br>
2025-10-11 13:44:19 -0300

    processar mensagem da fila

---
+++ process-lambda/app.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+from datetime import datetime
+
+rekognition = boto3.client('rekognition')
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+sqs = boto3.client('sqs')
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        try:
+            # Processar mensagem da fila
+            message = json.loads(record['body'])
+            request_id = message['requestId']
+            s3_key = message['s3Key']
+            user_email = message['userEmail']
